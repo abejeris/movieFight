288c2654db9e6ed0dedfbd80d3817ca0e5a0e153
Audrius Bejeris <audriusbejeris@gmail.com>
2024-04-24 20:20:12 +0300

    added fetch functionality

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hello');
+const fetchdata = async (searchTerm) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: '6ba6699',
+			s: searchTerm,
+		},
+	});
+	console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = (e) => {
+	if (timeoutId) {
+		clearTimeout(timeoutId);
+	}
+	timeoutId = setTimeout(() => {
+		fetchdata(e.target.value);
+	}, 500);
+};
+input.addEventListener('input', onInput);
