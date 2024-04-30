40dfaf0b95d81ddef97c027a24f036a9b061272b
Audrius Bejeris <audriusbejeris@gmail.com>
2024-04-30 16:30:10 +0300

    refactoring movie to items and adding fetch function to an object

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-const fetchData = async (searchTerm) => {
-	const response = await axios.get('http://www.omdbapi.com/', {
-		params: {
-			apikey: '6ba6699',
-			s: searchTerm,
-		},
-	});
-
-	if (response.data.Error) {
-		return [];
-	}
-	return response.data.Search;
-};
-
 createAutoComplete({
 	root: document.querySelector('.autocomplete'),
 	renderOption: (movie) => {
@@ -24,6 +10,19 @@ createAutoComplete({
 	},
 	inputValue: (movie) => {
 		return movie.Title;
+	},
+	fetchData: async (searchTerm) => {
+		const response = await axios.get('http://www.omdbapi.com/', {
+			params: {
+				apikey: '6ba6699',
+				s: searchTerm,
+			},
+		});
+
+		if (response.data.Error) {
+			return [];
+		}
+		return response.data.Search;
 	},
 });
 
